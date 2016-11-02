a299d69f3b84ba96cdfd220fc1ab8a6782ceca49
Cameron Alexander <emptyflash@gmail.com>
2016-11-02 15:51:17 -0500

    Add docs for dynamic routes, router, and main module. Also add doctest for dynamic routes

--- lib/heimdall.ex
+++ lib/heimdall.ex
@@ -1,2 +1,8 @@
 defmodule Heimdall do
+  @moduledoc Regex.replace(
+    ~r/```(elixir|json)(\n|.*)```/Us, 
+    File.read!("README.md"), 
+    fn(_, _, code) -> 
+      Regex.replace(~r/^/m, code, "    ") 
+    end)
 end
