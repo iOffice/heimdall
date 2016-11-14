d69e8135f4ce13350ecefac51a09efdd6602c690
Cameron Alexander <emptyflash@gmail.com>
2016-11-14 15:38:49 -0600

    Add marathon health check for /heimdall-health-check

--- lib/router.ex
+++ lib/router.ex
@@ -1,6 +1,6 @@
 defmodule Heimdall.Router do
   @moduledoc """
-  The applications router plug. 
+  The applications router plug.
 
   Forwards Marathon callback events coming in from /marathon-callback
   to `Heimdall.Marathon.BingeWatch` which will update the dynamic routes.
@@ -11,12 +11,19 @@ defmodule Heimdall.Router do
   use Plug.Router
   alias Heimdall.Marathon.BingeWatch
   alias Heimdall.DynamicRoutes
+  import Rackla
 
   plug :match
   plug :dispatch
 
 
   forward "/marathon-callback", to: BingeWatch
+
+  get "/heimdall-health-check" do
+    Application.fetch_env!(:heimdall, :marathon_url) <> "/ping"
+    |> request
+    |> response
+  end
 
   forward "/", to: DynamicRoutes, tab: :heimdall_routes
 
