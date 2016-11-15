09cc153c69f1dfef327779a410adccee1fc14c12
Cameron Alexander <emptyflash@gmail.com>
2016-11-15 10:01:34 -0600

    Bump patch version number with critical path bug fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Heimdall.Mixfile do
   def project do
     [
       app: :heimdall,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
