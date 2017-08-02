c0ab05e22fd1eb54897e1fa26baf35efd25501e2
Cameron Alexander <emptyflash@gmail.com>
2017-08-02 12:54:25 -0500

    Add before all filters and some docs

--- config/dev.exs
+++ config/dev.exs
@@ -4,4 +4,5 @@ config :heimdall,
   marathon_url: "http://localhost:8080",
   default_forward_url: "http://localhost/",
   register_marathon: false,
-  port: 4000
+  port: 4000,
+  filter_before_all: []
--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,5 @@ config :heimdall,
   marathon_url: "http://localhost:8889",
   default_forward_url: "http://localhost:8082",
   register_marathon: false,
-  port: 4000
+  port: 4000,
+  filter_before_all: []
